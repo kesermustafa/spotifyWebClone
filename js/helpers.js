10c23b5c6142ee02ef3a920013d559ef7a5d9022
Mustafa Keser <keserzade@gmail.com>
2024-04-17 20:32:12 +0300

    update

---
+++ js/helpers.js
@@ -0,0 +1,13 @@
+export const elements = {
+    list: document.querySelector(".list"),
+    audio: document.querySelector("audio"),
+    playingInfo: document.querySelector(".playing .info"),
+    audioSource: document.querySelector("audio source"),
+    form: document.querySelector("form"),
+    title: document.querySelector(".songs h2"),
+    menu: document.querySelector("#menu"),
+    ulList: document.querySelector("ul"),
+};
+console.log(elements.ulList);
+
+
